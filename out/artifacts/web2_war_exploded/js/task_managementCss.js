033996b1ccede72ddace37e3d4f25cb78ab2cf0f
982456770 <982456770@qq.com>
2020-04-19 14:36:27 +0800

    Revert "test"
    This reverts commit 20e18774

--- out/artifacts/web2_war_exploded/js/task_managementCss.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $width = $(document).width();
-    $height = $(document).height();
-    $(".nav_content").css({
-        "height": $height-$(".head").height()-35+"px"
-    })
-
-})
